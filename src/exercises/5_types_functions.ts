a8862b28bab929e53ea1fadf6f42f0510d28f4d4
alexsh882 <alexsh882@gmail.com>
2024-09-02 10:47:59 -0300

    feat: Add function for adding two numbers
    This commit adds a new file `5_types_functions.ts` which contains a function `add` that accepts two numbers as arguments and returns their sum. The function is properly typed with the parameter types and return type specified.

---
+++ src/exercises/5_types_functions.ts
@@ -0,0 +1,8 @@
+/* Función con tipado
+Escribe una función add que acepte dos números como argumentos y devuelva su suma. 
+Asegúrate de tipar adecuadamente los parámetros y el valor de retorno. */
+
+// Declaro una función add que recibe dos parámetros de tipo number y devuelve un valor de tipo number.
+function add(a: number, b: number): number {
+  return a + b;
+}
