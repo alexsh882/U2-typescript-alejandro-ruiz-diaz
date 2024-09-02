8b5260e6f702540eced27606e9d933b3c8652fb6
alexsh882 <alexsh882@gmail.com>
2024-09-02 10:50:02 -0300

    feat: Add typing for greet function
    This commit adds a new file `6_typing_functions.ts` which contains a function `greet` that takes a `name` argument of type string and returns a greeting message using that name. The function is properly typed with the parameter type specified.

---
+++ src/exercises/6_typing_functions.ts
@@ -0,0 +1,7 @@
+/* Tipado en funciones
+Crea una función greet que tome un argumento name de tipo string y devuelva una frase de saludo usando ese nombre. */
+
+
+function greet(name: string) {
+    return `Welcome ${name}, to code with ts!`;
+}
