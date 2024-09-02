305e62db59369ea430a01f561626f45d39fed4a6
alexsh882 <alexsh882@gmail.com>
2024-09-02 10:59:17 -0300

    feat: Update Car class implementation and usage
    This commit modifies the implementation of the `Car` class in the `7_basic_class.ts` file. The `make` property is now initialized with a default value of 'Unknown make'. Additionally, the `drive()` method has been updated to use template literals for improved readability. The purpose of this commit is to enhance the `Car` class and demonstrate its usage by creating an instance of the class with the make set to 'Toyota' and calling the `drive()` method.

--- src/exercises/7_basic_class.ts
+++ src/exercises/7_basic_class.ts
@@ -1,20 +1,20 @@
+
 /* Clase básica
     Define una clase Car que tenga una propiedad make (marca del coche) de tipo string y un método drive() 
     que imprima un mensaje en la consola diciendo "Driving a [make]". */
-
-    
+ 
+// Defino la clase Car con una pública propiedad make de tipo string y un método drive que imprime un mensaje en la consola.
 class Car {
-    make: string;
-
-    constructor(make: string) {
-        this.make = make;
-    }
-
+    public make: string = 'Unknown make';    
     drive() {
         console.log(`Driving a ${this.make}`);
     }
 }
 
-const car = new Car('Toyota');
+// Creo una instancia de la clase Car.
+const car = new Car();
 
-car.drive(); // la ejecución de este método de la clase Car daría como resultado la impresión en controla de "Driving a Toyota"
+// asigno un valor a la propiedad pública make de la instancia car.
+car.make = 'Toyota';
+
+car.drive(); // la ejecución de este método de la instancia "car" daría como resultado la impresión en controla de "Driving a Toyota"
